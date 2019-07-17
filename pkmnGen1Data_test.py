c8b5c796afef9750842967bce4fdfc5d02c74e08
dlsimpao <dlsimpao@yahoo.com>
2019-07-16 17:03:25 -0700

    Update pkmnGen1Data_test.py

--- pkmnGen1Data_test.py
+++ pkmnGen1Data_test.py
@@ -1,34 +1,59 @@
 #Pokemon Matchup test generator
 #FOR GEN I ONLY
 
+import TypeMatchups as tm
+
 #later include choice between a combination of generations and all
 #include other gen csv files and also master file
 
 genFile = "PGen1.csv"
 
 #dictionaries with values based on 
-gen1Pokenum_dict = {}
+#not in use#gen1Pokenum_dict = {}
 gen1Pokename_dict = {}
 
-num, name, hp, atk, dfn, spa, spd, tot, type1, type2 = 0,0,0,0,0,0,0,0,0,0
+
 
 fHandle = open(genFile, "r")
-fHandle.readline()
+header = fHandle.readline()
+header = header.strip('\n')
+header = header.split(',')
+
 for line in fHandle:
 
     line = line.strip('\n')
-    #print(line)
 
     #list of Pokemon's stats
-    line = line.split(',',2)
-    print(line)
-    
-    gen1Pokenum_dict[line[0]] = [line[2],line[3],line[4],line[5],line[6],line[7],line[8]]
-    gen1Pokename_dict[line[1]] = [line[2],line[3],line[4],line[5],line[6],line[7],line[8]]
-#print(line)
+    line = line.split(',')
 
-for key in gen1Pokenum_dict:
+    #dictionary of dictionaries
+    
+    #not in use#gen1Pokenum_dict[line[0]] = [line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9]]
+    gen1Pokename_dict[line[1]] = {header[0]:line[0],header[2]:line[2],header[3]:line[3],header[4]:line[4],\
+                                  header[5]:line[5],header[6]:line[6],header[7]:line[7],header[8]:line[8],\
+                                  header[9]:line[9], header[10]:line[10]}
+
+
+for key in gen1Pokename_dict:
     print(key)
+
+pokeName = "charizard".title()
+pokeTypes = [gen1Pokename_dict[pokeName]["Type I"],gen1Pokename_dict[pokeName]["Type II"]]
+
+if pokeName in gen1Pokename_dict:
+    printStr = ""
+    if pokeTypes[1] == "":
+        printStr = pokeName+" is a "+pokeTypes[0]+" type Pokemon"
+    else:
+        printStr = pokeName+" is a "+pokeTypes[0]+"-"+pokeTypes[1]+\
+                   " Dual type Pokemon."
+print(printStr)
+
+#specify which Pokemon generation
+pokeGen = 1
+
+tm.printEffectReport(pokeTypes)
+
 
 
 
