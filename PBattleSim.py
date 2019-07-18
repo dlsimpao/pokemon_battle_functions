eb244646a4ebc5aa5c1b5b61376f122fa926fa7f
dlsimpao <dlsimpao@yahoo.com>
2019-07-18 09:21:24 -0700

    Update PBattleSim.py

--- PBattleSim.py
+++ PBattleSim.py
@@ -3,6 +3,7 @@
 import PMonsterClass as pmc
 import TypeMatchups as tm
 import pkmnTeamGenerator as ptg
+import conventionalCode as cc
 
 def main():
     #handle string containing Pokemon types from all nations
@@ -20,27 +21,32 @@ def main():
     oppPokeParty = ["Weavile","Sneasel","Krabby","Starly","Leafeon","Basculin"]
 
     #printParty(myPokeParty,natPokeType_dict)
-    optimizeParty(myPokeParty,oppPokeParty,natPokeType_dict)
+    #stores the optimal party setup, assuming no switches and advantage based solely on type
+
+    #Mine
+    myOptParty = optimizeParty(myPokeParty,oppPokeParty,natPokeType_dict)
+    strongOpp = identifyStrongOpp(myOptParty, oppPokeParty)
+
+    #Opp
+    oppOptParty = optimizeParty(oppPokeParty, myPokeParty,natPokeType_dict)
+    strongMine = identifyStrongOpp(oppOptParty, myPokeParty)
+    
+    printFavorMatch(myOptParty, strongMine)
+    cc.space(2)
+    printAvoidMatch(oppOptParty)
+
+    
     
 
-    
-
-
-
-#strips newline and splits at ','
-def strNspl(line):
-    line = line.strip("\n")
-    line = line.split(",")
-    return line
 
 #opens Pokemon Database
 def createTypeDict(handleStr):
     pokeType_dict = {}
     fHandle = open("PGenAll170719.csv")
     header = fHandle.readline()
-    header = strNspl(header)
+    header = cc.strNspl(header)
     for line in fHandle:
-        line = strNspl(line)
+        line = cc.strNspl(line)
         pokeType_dict[line[0]] = [line[1],line[2]]
     return pokeType_dict
 
@@ -70,18 +76,18 @@ def optimizeParty(party_list,opp_list,type_dict):
     print("My Pokemon: "+str(myTypes_dict))
     for mon in opp_list:
         oppTypes_dict[mon] = getType(mon,type_dict)
-    space()
+    cc.space(3)
     print("Opposing Pokemon: "+str(oppTypes_dict))
 
     #for each pokemon in opponents party, find their matchup effectiveness
     for mon in oppTypes_dict:
         immuneAgainst, superResistantAgainst, resistantAgainst, weakAgainst,superWeakAgainst = tm.getEffectMatch(oppTypes_dict[mon])
-        effect_list = tm.capLists([immuneAgainst, superResistantAgainst, resistantAgainst, weakAgainst,superWeakAgainst])
+        effect_list = cc.capLists([immuneAgainst, superResistantAgainst, resistantAgainst, weakAgainst,superWeakAgainst])
         oppTypesMatchup[mon] = effect_list
-    space()
+    cc.space(3)
     
     print(oppTypesMatchup)
-    space()
+    cc.space(3)
     #print(oppTypesMatchup["Sneasel"][4])
     #given each opposing pokemon's weakness, arrange my party strategically
     #for each pokemon in opponent's party
@@ -124,13 +130,41 @@ def optimizeParty(party_list,opp_list,type_dict):
                             oppList2 += [oppMon]
                         #adds to dictionary
                         myOptLineUp[myMon] = oppList2
-    print(myOptLineUp)#works
+    #print(myOptLineUp)#works
+    return myOptLineUp
     #NEXT: normal opposition, matchups to avoid
     #NEXT: get and print functions
-    
 
-def space():
-    print("\n\n\n")
+#identifies which opposing Pokemon will go evenly or better than mine
+#given my optimal lineup dictionary and opponents' list
+def identifyStrongOpp(myOpt_dict, oppList):
+    retList = oppList
+    #for each opposing pokemon
+    for oppMon in oppList:
+        for mon in myOpt_dict:
+            #if any of my Pokemon has an advantage over opposing, remove from retList
+            if oppMon in myOpt_dict[mon]:
+                #remove if valid
+                if oppMon in retList:
+                    #if Pokemon in retList, replace with ""
+                    retList = ["" if x == oppMon else x for x in retList]
+    retList = cc.retNonBlanks(retList)
+    return retList
+
+#prints desired matchups
+def printFavorMatch(myOpt_dict, strongPoke):
+    for mon in myOpt_dict:
+        print("My "+mon+": Use against:"+str(myOpt_dict[mon]))
+    print("Opposing team has no type counter for"+str(strongPoke))
+
+
+#prints unwanted matchups
+def printAvoidMatch(oppOpt_dict):
+    for mon in oppOpt_dict:
+        print("Opposing "+mon+": Do not use "+str(oppOpt_dict[mon]))
+
+
+
         
     
 main()
