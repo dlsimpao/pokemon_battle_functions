d3c58f57f5cb05d6e42fcb26205f513bdcf90442
dlsimpao <dlsimpao@yahoo.com>
2019-07-18 16:38:50 -0700

    Update conventionalCode.py

--- conventionalCode.py
+++ conventionalCode.py
@@ -18,11 +18,41 @@ def retNonBlanks(alist,blank = ""):
             retList.append(element)
     return retList
 
-#function takes a list of sublists of strings and capitalizes the first letter each string in the sublist
+#function takes a list of sublists of strings and sets the proper case for each string in the sublist
 #returns a list of sublists
-def capLists(alist):
+def caseLists(alist, case):
     retList = []
     for sublist in alist:
-        sublist = [x.title() for x in sublist]
+        if case == "title":
+            sublist = [x.title() for x in sublist]
+        elif case == "lower":
+            sublist = [x.lower() for x in sublist]
+        elif case == "upper":
+            sublist = [x.upper() for x in sublist]
+        else:
+            raise ValueError("Enter correct parameters")
         retList.append(sublist)
     return retList
+
+#function turns lists of lists into a list of elements
+#works only with one layer of list
+def cascadeLists(listOfLists):
+    retList = []
+    for element in listOfLists:
+        if not isinstance(element,list):
+            retList.append(element)
+        else:
+            retList.extend([*element])
+    return retList 
+
+#recursively delists lists through any number of layers
+def rec_cascadeLists(listOfLists):
+    #checks if False is in the list comprehension
+    #list comprehension checks each element in list to see if it's a list, returns False if list
+    if False not in [True if not isinstance(x,list) else False for x in listOfLists]:
+        return listOfLists
+    else:
+        return rec_cascadeLists(cascadeLists(listOfLists))
+
+#print(rec_cascadeLists([1,2,3,[4,5,[6]]]))
+
