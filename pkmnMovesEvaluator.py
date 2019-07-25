a65e1ec867dfed8ccc8cdd9e0aa534646c51c25a
dlsimpao <dlsimpao@yahoo.com>
2019-07-25 10:37:39 -0700

    Update pkmnMovesEvaluator.py

--- pkmnMovesEvaluator.py
+++ pkmnMovesEvaluator.py
@@ -18,7 +18,7 @@ def main():
 ##        print(pkmnMoveType_dict[key])
     #moves = ptg.genPokeMoves(4,["normal","fire"],["Physical","Special"])
     #moves = ptg.genPokeMoves(4,["normal","fire"],["Status"])
-    moves = ptg.genPokeMoves(4,["normal","fire"])
+    moves = ptg.genPokeMoves(4,["water","fire"],["Physical"])
     moveEvals = getMoveEvals(moves,pkmnMoveType_dict,types_dict)
     printMoveEvals(moveEvals)
     
