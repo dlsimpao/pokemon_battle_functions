429825581df354ef102b30074e9cb7d6794f08a8
dlsimpao <dlsimpao@yahoo.com>
2019-07-25 13:31:41 -0700

    Update pkmnTeamGenerator.py

--- pkmnTeamGenerator.py
+++ pkmnTeamGenerator.py
@@ -1,7 +1,7 @@
 #Pokemon party generator
 #possible additions
 #1. Optimized Pokemon party gen
-#2. Guided Pokemon party creator
+#2. Guided Pokemon party creator, given initial user choices, recommends next choice
 #3. Themed Party gen
 
 
@@ -14,10 +14,71 @@ import PMonsterClass as pmc
 import random
 
 class pkmnTeam():
-    def __init__(self, pkmn_list):
-        self.pkmn_list = pkmn_list
+    pkmn_list = []
+    def __init__(self, pkmn_list = []):
+        notRec = []
+        for i in pkmn_list:
+            if self.isValidName(i,"Pokemon"):
+                self.pkmn_list.append(i)
+            else:
+                notRec.append(i)
+        if notRec != []:
+            print("The Pokedex does not recognize the name/s: "+str(notRec))
         self.partyCount = len(pkmn_list)
 
+    def addMember(self, pkmnToAdd):
+        if self.isValidName(pkmnToAdd):
+            if self.partyCount < 6:
+                temp = 
+                self.pkmn_list.append(pkmnToAdd)
+                self.partyCount += 1
+            else:
+                self.printParty()
+                pkmnToSwap = input("Your party is full. Please select a Pokemon to swap.\n")
+                if pkmnToSwap in self.pkmn_list:
+                    self.swapMember(pkmnToAdd,pkmnToSwap)
+                else:
+                    self.addMember(pkmnToAdd)
+        else:
+            self.printNotRec()
+
+    def swapMember(self, pkmnIn, pkmnOut):
+        if pkmnIn != pkmnOut:
+            self.pkmn_list.remove(pkmnOut)
+            self.partyCount -= 1
+            self.addMember(pkmnIn)
+        else:
+            print(pkmnIn.title()+" will not be added.")
+
+    def printNotRec(self):
+        print("The Pokedex does not recognize this name.")
+        
+        
+    def isValidName(self, name, nameCategory = "Pokemon"):
+        nameCategory = nameCategory.title()
+        valid = False
+        try:
+            fHandle = pd.getFHandle(nameCategory)
+        except:
+            raise(ValueError)
+        pkmnNames = pd.autoDict(fHandle,index = False)
+        if name in pkmnNames:
+            valid = True
+        return valid
+            
+        pkmnName_list
+    def randomizeParty(self,num):
+        self.pkmn_list = genPokeTeam(num)
+
+    def getParty(self):
+        return self.pkmn_list
+
+    def printParty(self):
+        party = self.getParty()
+        for mon in party:
+            print(mon)
+
+#add restrictions like type, gen
 def genPokeTeam(num):
     pokeTeam_list = []
     pokeName_dict = {}
@@ -98,4 +159,7 @@ def createRandList(num, d, unique = True):
 
 ##alist = genPokeTeam(6)
 ##print(alist)
-
+Red = pkmnTeam(["Heatran","Gallade","Zigzagoon","Entei","Espeon","Rattata"])
+#Red.randomizeParty(6)
+Red.addMember("Charizard")
+Red.printParty()
