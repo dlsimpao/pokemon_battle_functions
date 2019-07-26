79fcc81db2a35907625367b6fb66439c7207b9d8
dlsimpao <dlsimpao@yahoo.com>
2019-07-26 09:27:28 -0700

    Update conventionalCode.py

--- conventionalCode.py
+++ conventionalCode.py
@@ -1,22 +1,44 @@
 #Conventional user-defined functions
 
 #strips newline and splits at ','
-def strNspl(line):
-    line = line.strip("\n")
-    line = line.split(",")
+def strNspl(line,strp = "\n",splt = ","):
+    line = line.strip(strp)
+    line = line.split(splt)
     return line
 
-#adds a space/s for printing
+#removes specific characters from a list of strings
+def rem(strList, char):
+    retList = []
+    for i in strList:
+        i = i.strip(char = "")
+        retList.append(i)
+    return retList
+
+#checks if string has no empty spaces or white spaces
+def isSuperEmpty(string):
+    superEmpty = False
+    if string.isspace():
+        superEmpty = True
+    elif string == "":
+        superEmpty = True
+    return superEmpty
+
+#adds a newline for printing
 def space(num):
     print("\n"*num)
 
-#returns all non-blank in  list
+#returns all non-blank in list
 def retNonBlanks(alist,blank = ""):
     retList = []
-    for element in alist:
-        if element != blank:
-            retList.append(element)
+    [retList.append(x) if x != blank else x for x in alist]
     return retList
+
+#test
+
+##a = [1,2,3,"",4,5,""," "]
+##b = retNonBlanks(a,"")
+##print(b)
+
 
 #function takes a list of sublists of strings and sets the proper case for each string in the sublist
 #returns a list of sublists
@@ -80,11 +102,14 @@ def rec_cascadeTuples(tupOfTups):
 
 #prints relative counts
 def printRel(count_dict):
-    totCount = sum(count_dict.values())
+    total = sum(count_dict.values())
     retDict = {}
+    print("{:<20}{:<40}{:<15}".format("Key","Relative frequency","Counts"))
     for i in count_dict:
-        print("{:<20}".format(str(i))+"\t{:^15}".format("{:.4f}".format((count_dict[i]/totCount)),3)+"\t"+\
-              "{:^15}".format(str(count_dict[i])))
+        print("{:<20}{:<40}{:<15}".format(str(i),str(count_dict[i]/total),str(count_dict[i])))
+#test
+count_test = {"Hello":1,"Hi":2,"Sup":3}
+##printRel(count_test)
 
 #prints Horizontal bar charts
 def printHorBarChart(count_dict):
@@ -92,12 +117,13 @@ def printHorBarChart(count_dict):
     barStr = ""
     for i in count_dict:
         barStr = bar*count_dict[i]
-        print("{:<20}".format(str(i))+"\t{:<30}".format(barStr))
+        print("{:<20}{:<30}".format(str(i),barStr))
+#printHorBarChart(count_test)
 
 #lazy method, not efficient
-def printTop(num, count_dict):
+def printTop(num, count_dict, rev = True):
     top_list = [*count_dict.values()]
-    top_list.sort(reverse = True)
+    top_list.sort(reverse = rev)
     top_list = top_list[:num]
 
     retDict = {}
@@ -106,7 +132,9 @@ def printTop(num, count_dict):
         for i in count_dict:
             if count_dict[i] == j and i not in retDict:
                 #retDict[i] = j
-                print("{:<20}".format(str(i))+"\t{:<30}".format(j))
+                print("{:<20}{:<30}".format(str(i),j))
+
+#printTop(3,count_test)
 
     
 
