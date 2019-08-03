083cf48d899ef4b06a03c2011005decc162b16cc
dlsimpao <dlsimpao@yahoo.com>
2019-08-03 00:25:27 -0700

    Update pkmnTeamEvaluator.py

--- pkmnTeamEvaluator.py
+++ pkmnTeamEvaluator.py
@@ -5,32 +5,47 @@ import pkmnData as pd
 import conventionalCode as cc
 import pkmnTypeMatchups as tm
 
+#create a scoring system that depicts favor to win
+
 def main():
     pt_dict = pd.pkmnTypes_dict
     penguin = ptg.pkmnTeam(["Blitzle", "Sudowoodo", "Wormadam (T)", "Mareanie", "Shiinotic","Porygon2"])
 ##    penguin.randomizeParty(6)
     print("Penguin\n")
-    penguin.printParty()
+    #penguin.printParty()
 
     #figure out how to generate random pokemon of the Pokemon's type and set it as their moveset
     for mon,obj in penguin.getParty().items():
         types = [x.upper() for x in pt_dict[mon]]
         move_list = ptg.genPokeMoves(4,types, cat = ["Physical","Special"]) 
-        print(types)
         for move in move_list:
-            obj.setMove(mon)
+            obj.setMove(move)
         
     cc.space(1)
+    
     myPkmn_dict = penguin.getParty()
     for mon,obj in myPkmn_dict.items():
         print(obj)
+        cc.space(1)
     
     cc.space(1)
     
     ostrich = ptg.pkmnTeam(["Lotad", "Croagunk", "Pikipek", "Nidoran?", "Spinda", "Articuno"])
 ##    ostrich.randomizeParty(6)
     print("Ostrich\n")
-    ostrich.printParty()
+    #ostrich.printParty()
+
+    #cc.space(1)
+
+    for mon,obj in ostrich.getParty().items():
+        types = [x.upper() for x in pt_dict[mon]]
+        move_list = ptg.genPokeMoves(4,types, cat = ["Physical","Special"]) 
+        for move in move_list:
+            obj.setMove(move)
+
+    for mon,obj in myPkmn_dict.items():
+        print(obj)
+        cc.space(1)
 
 
 main()
