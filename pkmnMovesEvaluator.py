4cdab89f3847fc43c9c3f2a67f113ce19e1705ed
dlsimpao <dlsimpao@yahoo.com>
2019-08-02 23:57:02 -0700

    Update pkmnMovesEvaluator.py

--- pkmnMovesEvaluator.py
+++ pkmnMovesEvaluator.py
@@ -1,40 +1,37 @@
-#Pokemon party evaluator
-#evaluates the party based 
-import conventionalCode as cc
-import pkmnData as pd
+#pkmnTeamEvaluator
 import pkmnTeamGenerator as ptg
-import TypeMatchups as tm
+import pkmnBattleMatchups as pbm
+import pkmnData as pd
+import conventionalCode as cc
+import pkmnTypeMatchups as tm
 
-
-#include physical, status effects
 def main():
-    fHandle = pd.getFHandle("Moves")
-    #print(pd.getHeader(fHandle))
-    #creates a dictionary, name as key, type and cat. as values
-    pkmnMoveType_dict = pd.autoDict(fHandle, ["Name","Type","Cat."],index = False)
-    types_dict = pd.getTypes_dict()
+    pt_dict = pd.pkmnTypes_dict
+    penguin = ptg.pkmnTeam(["Blitzle", "Sudowoodo", "Wormadam (T)", "Mareanie", "Shiinotic","Porygon2"])
+##    penguin.randomizeParty(6)
+    print("Penguin\n")
+    penguin.printParty()
 
-##    for key in pkmnMoveType_dict:
-##        print(pkmnMoveType_dict[key])
-    #moves = ptg.genPokeMoves(4,["normal","fire"],["Physical","Special"])
-    #moves = ptg.genPokeMoves(4,["normal","fire"],["Status"])
-    moves = ptg.genPokeMoves(4,["water","fire"])
-    moveEvals = getMoveEvals(moves,pkmnMoveType_dict,types_dict)
-    printMoveEvals(moveEvals)
+    #figure out how to generate random pokemon of the Pokemon's type and set it as their moveset
+    for mon,obj in penguin.getParty().items():
+        types = [x.upper() for x in pt_dict[mon]]
+        move_list = ptg.genPokeMoves(4,types, cat = ["Physical","Special"]) 
+        print(types)
+        for move in move_list:
+            obj.setMove(mon)
+        
+    cc.space(1)
+    myPkmn_dict = penguin.getParty()
+    for mon,obj in myPkmn_dict.items():
+        print(obj)
     
-#receives a list of moves and evaluates it, returning a dictionary (key = move, value = list of effect matchups)
-        
-def getMoveEvals(moves_list, pkmnMT_dict,types_dict):
-    evalMoves_dict = {}
-    for move in moves_list:
-        evalMoves_dict[(pkmnMT_dict[move][1],move)] = tm.getMoveTypeEffects(pkmnMT_dict[move][0],types_dict)
-    return evalMoves_dict
+    cc.space(1)
+    
+    ostrich = ptg.pkmnTeam(["Lotad", "Croagunk", "Pikipek", "Nidoran?", "Spinda", "Articuno"])
+##    ostrich.randomizeParty(6)
+    print("Ostrich\n")
+    ostrich.printParty()
 
-
-def printMoveEvals(mEvals):
-    print("{:<45}{:<45}{:<45}{:<45}".format("Category || Moves","has no effect","not very effective","super effective"))
-    for key in mEvals:
-        print("{:<45}{:<45}{:<45}{:<45}".format(str(key),str(mEvals[key][0]),str(mEvals[key][1]),str(mEvals[key][2])))
-        
 
 main()
+    
